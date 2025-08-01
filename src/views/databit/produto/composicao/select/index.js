0a5181332442af0a6225a8bc707f8b0f5da94b8f
GeorgeSilva <projetos04@databit.com.br>
2025-08-01 16:31:34 -0300

    comp

--- src/views/databit/produto/composicao/select/index.js
+++ src/views/databit/produto/composicao/select/index.js
@@ -39,35 +39,6 @@ const Composicaoselec = (props) => {
   const [showmapa, setShowmapa] = React.useState(false);
   const [location, setLocation] = React.useState([]);
 
-  const states = [
-    { value: 'AC', label: 'Acre (AC)' },
-    { value: 'AL', label: 'Alagoas (AL)' },
-    { value: 'AP', label: 'Amapá (AP)' },
-    { value: 'AM', label: 'Amazonas (AM)' },
-    { value: 'BA', label: 'Bahia (BA)' },
-    { value: 'CE', label: 'Ceará (CE)' },
-    { value: 'DF', label: 'Distrito Federal (DF)' },
-    { value: 'ES', label: 'Espírito Santo (ES)' },
-    { value: 'GO', label: 'Goiás (GO)' },
-    { value: 'MA', label: 'Maranhão (MA)' },
-    { value: 'MT', label: 'Mato Grosso (MT)' },
-    { value: 'MS', label: 'Mato Grosso do Sul (MS)' },
-    { value: 'MG', label: 'Minas Gerais (MG)' },
-    { value: 'PA', label: 'Pará (PA)' },
-    { value: 'PB', label: 'Paraíba (PB)' },
-    { value: 'PR', label: 'Paraná (PR)' },
-    { value: 'PE', label: 'Pernambuco (PE)' },
-    { value: 'PI', label: 'Piauí (PI)' },
-    { value: 'RJ', label: 'Rio de Janeiro (RJ)' },
-    { value: 'RN', label: 'Rio Grande do Norte (RN)' },
-    { value: 'RS', label: 'Rio Grande do Sul (RS)' },
-    { value: 'RO', label: 'Rondônia (RO)' },
-    { value: 'RR', label: 'Roraima (RR)' },
-    { value: 'SC', label: 'Santa Catarina (SC)' },
-    { value: 'SP', label: 'São Paulo (SP)' },
-    { value: 'SE', label: 'Sergipe (SE)' },
-    { value: 'TO', label: 'Tocantins (TO)' }
-  ];
 
   useEffect(() => {
     let tmpsteps = [];
@@ -87,313 +58,17 @@ const Composicaoselec = (props) => {
     setOptions(tmpoptions);
 
     setFields([
-      {
-        id: 0,
-        campo: 'equipamemto',
-        funcao: 'Serial',
-        tipo: 'varchar',
-        nome: 'equipamemto',
-        tamanho: 20,
-        tipoobject: 1,
-        widthfield: 12,
-        measure: '12rem',
-        disabled: valuesdisable[0]
-      },
-      {
-        id: 1,
-        campo: 'produto',
-        funcao: 'Produto',
-        tipo: 'varchar',
-        nome: 'produto',
-        tamanho: 5,
-        tipoobject: 2,
-        widthfield: 39,
-        measure: '39rem',
-        tabelaref: 'TB01010',
-        widthname: 30,
-        disabled: valuesdisable[0]
-      },
-      {
-        id: 2,
-        campo: 'referencia',
-        funcao: 'Referência',
-        tipo: 'varchar',
-        nome: 'referencia',
-        tamanho: 20,
-        tipoobject: 1,
-        widthfield: 18,
-        measure: '18rem',
-        disabled: valuesdisable[2]
-      },
-      {
-        id: 3,
-        campo: 'codbarras',
-        funcao: 'Código Barras',
-        tipo: 'varchar',
-        nome: 'codbarras',
-        tamanho: 20,
-        tipoobject: 1,
-        widthfield: 17,
-        measure: '17rem',
-        disabled: valuesdisable[3]
-      },
-      {
-        id: 4,
-        campo: 'codauxiliar',
-        funcao: 'Código Auxiliar',
-        tipo: 'varchar',
-        nome: 'codauxiliar',
-        tamanho: 20,
-        tipoobject: 1,
-        widthfield: 16,
-        measure: '16rem',
-        disabled: valuesdisable[4]
-      },
-      {
-        id: 5,
-        campo: 'nomemarca',
-        funcao: 'Marca',
-        tipo: 'varchar',
-        nome: 'marca',
-        tamanho: 60,
-        tipoobject: 1,
-        widthfield: 13,
-        measure: '13rem',
-        disabled: valuesdisable[5]
-      },
-      {
-        id: 6,
-        campo: 'dtinstalacao',
-        funcao: 'Data Instalação',
-        tipo: 'varchar',
-        nome: 'dtinstalacao',
-        tamanho: 60,
-        tipoobject: 5,
-        widthfield: 10,
-        measure: '10rem',
-        disabled: valuesdisable[6]
-      },
-      {
-        id: 7,
-        campo: 'dtvalidade',
-        funcao: 'Data Validade',
-        tipo: 'varchar',
-        nome: 'dtvalidade',
-        tamanho: 60,
-        tipoobject: 5,
-        widthfield: 10,
-        measure: '10rem',
-        disabled: valuesdisable[7]
-      },
-      {
-        id: 8,
-        campo: 'pat',
-        funcao: 'Nº Patrimônio',
-        tipo: 'pat',
-        nome: 'prazo',
-        tamanho: 20,
-        tipoobject: 1,
-        widthfield: 10,
-        measure: '10rem',
-        disabled: valuesdisable[8]
-      },
-      {
-        id: 23,
-        campo: 'libleitura',
-        funcao: 'Ler Medidores',
-        tipo: 'varchar',
-        nome: 'libleitura',
-        tamanho: 20,
-        tipoobject: 11,
-        widthfield: 8,
-        measure: '8rem',
-        itens: 'Não,Sim',
-        values: 'N,S',
-        disabled: valuesdisable[23]
-      }
+      { id: 0, campo: 'codigo', funcao: 'Código', tipo: 'varchar', nome: 'produto', tamanho: 5, tipoobject: 1, widthfield: 6, measure: '6rem', disabled: true },
+      { id: 1, campo: 'referencia', funcao: 'Referência', tipo: 'varchar', nome: 'referencia', tamanho: 20, tipoobject: 1, widthfield: 15, measure: '21rem', disabled: true },
+      { id: 2, campo: 'codbarras', funcao: 'Código Barras', tipo: 'varchar', nome: 'codbarras', tamanho: 20, tipoobject: 1, widthfield: 15, measure: '15rem', disabled: true },
+      { id: 3, campo: 'codauxiliar', funcao: 'Código Auxiliar', tipo: 'varchar', nome: 'codauxiliar', tamanho: 20, tipoobject: 1, widthfield: 15, measure: '15rem', disabled: true },
+      { id: 4, campo: 'unprod', funcao: 'UN', tipo: 'varchar', nome: 'unprod', tamanho: 60, tipoobject: 1, widthfield: 13, measure: '5rem', disabled: true },
+      { id: 5, campo: 'qtde', funcao: 'Qtde', tipo: 'varchar', nome: 'marca', tamanho: 60, tipoobject: 1, widthfield: 13, measure: '5rem', disabled: true },
+      { id: 6, campo: 'nome', funcao: 'Descrição Produto', tipo: 'varchar', nome: 'nomeproduto', tamanho: 60, tipoobject: 1, widthfield: 51, measure: '45rem', disabled: true },
+      { id: 7, campo: 'grupo', funcao: 'Grupo', tipo: 'varchar', nome: 'grupo', tamanho: 60, tipoobject: 1, widthfield: 10, measure: '11rem', disabled: true, decimal: 2 },
+      { id: 8, campo: 'subgrupo', funcao: 'Subgrupo', tipo: 'varchar', nome: 'subgrupo', tamanho: 60, tipoobject: 1, widthfield: 10, measure: '11rem', disabled: true },
     ]);
 
-    setFieldsobs([
-      {
-        id: 9,
-        campo: 'obs',
-        funcao: 'Observações',
-        tipo: 'text',
-        nome: 'obs',
-        tipoobject: 6,
-        tamanho: 10,
-        widthfield: 10,
-        measure: '10rem',
-        lines: 9,
-        disabled: valuesdisable[9]
-      }
-    ]);
-
-    setFieldsaddress([
-      {
-        id: 10,
-        campo: 'cep',
-        funcao: 'Cep',
-        tipo: 'varchar',
-        nome: 'campo',
-        tamanho: 10,
-        tipoobject: 8,
-        widthfield: 10,
-        measure: '8rem',
-        disabled: valuesdisable[10],
-        tipomascara: 3
-      },
-      {
-        id: 11,
-        campo: 'end',
-        funcao: 'Logradouro',
-        tipo: 'varchar',
-        nome: 'end',
-        tamanho: 60,
-        tipoobject: 1,
-        widthfield: 27,
-        measure: '27rem',
-        disabled: valuesdisable[11]
-      },
-      {
-        id: 12,
-        campo: 'num',
-        funcao: 'Nº',
-        tipo: 'int',
-        nome: 'num',
-        tamanho: 10,
-        tipoobject: 4,
-        widthfield: 8,
-        measure: '8rem',
-        disabled: valuesdisable[12]
-      },
-      {
-        id: 13,
-        campo: 'comp',
-        funcao: 'Compl.',
-        tipo: 'varchar',
-        nome: 'num',
-        tamanho: 10,
-        tipoobject: 1,
-        widthfield: 10,
-        measure: '10rem',
-        disabled: valuesdisable[13]
-      },
-      {
-        id: 14,
-        campo: 'bairro',
-        funcao: 'Bairro',
-        tipo: 'varchar',
-        nome: 'bairro',
-        tamanho: 30,
-        tipoobject: 1,
-        widthfield: 15,
-        measure: '15rem',
-        disabled: valuesdisable[14]
-      },
-      {
-        id: 15,
-        campo: 'cidade',
-        funcao: 'Cidade',
-        tipo: 'varchar',
-        nome: 'cidade',
-        tamanho: 30,
-        tipoobject: 1,
-        widthfield: 18,
-        measure: '18rem',
-        disabled: valuesdisable[15]
-      },
-      {
-        id: 16,
-        campo: 'estado',
-        funcao: 'Estado',
-        tipo: 'varchar',
-        nome: 'estado',
-        tamanho: 2,
-        tipoobject: 11,
-        widthfield: 14,
-        measure: '14rem',
-        options: states,
-        disabled: valuesdisable[16]
-      },
-
-      {
-        id: 17,
-        campo: 'fone',
-        funcao: 'Fone',
-        tipo: 'varchar',
-        nome: 'fone',
-        tamanho: 10,
-        tipoobject: 8,
-        widthfield: 12,
-        measure: '12rem',
-        disabled: valuesdisable[17],
-        tipomascara: 4
-      },
-      {
-        id: 18,
-        campo: 'foneaux',
-        funcao: 'Fone 2',
-        tipo: 'varchar',
-        nome: 'foneaux',
-        tamanho: 10,
-        tipoobject: 8,
-        widthfield: 12,
-        measure: '12rem',
-        disabled: valuesdisable[18],
-        tipomascara: 4
-      },
-      {
-        id: 19,
-        campo: 'whatsapp',
-        funcao: 'WhatsAPP',
-        tipo: 'varchar',
-        nome: 'fax',
-        tamanho: 10,
-        tipoobject: 8,
-        widthfield: 12,
-        measure: '12rem',
-        disabled: valuesdisable[19],
-        tipomascara: 5
-      },
-      {
-        id: 20,
-        campo: 'contato',
-        funcao: 'Contato',
-        tipo: 'varchar',
-        nome: 'contato',
-        tamanho: 30,
-        tipoobject: 1,
-        widthfield: 15,
-        measure: '15.5rem',
-        disabled: valuesdisable[20]
-      },
-      {
-        id: 21,
-        campo: 'email',
-        funcao: 'Email',
-        tipo: 'varchar',
-        nome: 'email',
-        tamanho: 200,
-        tipoobject: 1,
-        widthfield: 24,
-        measure: '24rem',
-        disabled: valuesdisable[21],
-        charnormal: true
-      },
-      {
-        id: 22,
-        campo: 'local',
-        funcao: 'Local de Instalação',
-        tipo: 'varchar',
-        nome: 'local',
-        tamanho: 30,
-        tipoobject: 1,
-        widthfield: 15,
-        measure: '15.5rem',
-        disabled: valuesdisable[22]
-      }
-    ]);
   }, []);
 
   useEffect(() => {
@@ -620,10 +295,10 @@ const Composicaoselec = (props) => {
       item['email'] = valuesfield[21];
       item['local'] = valuesfield[22];
       item['libleitura'] = valuesfield[23];
-      console.log(item);
+      //console.log(item);
       setCarregando(true);
       if (inclusao) {
-        apiInsert('ClienteEquipamento', item).then((response) => {
+        apiInsert('ProdutoComposicao', item).then((response) => {
           if (response.status === 200) {
             if (response.data.status === 1) {
               setShowselec(false);
@@ -635,7 +310,7 @@ const Composicaoselec = (props) => {
           }
         });
       } else {
-        apiUpdate('ClienteEquipamento', item).then((response) => {
+        apiUpdate('ProdutoComposicao', item).then((response) => {
           if (response.status === 200) {
             console.log(response.data);
             if (response.data.status === 1) {
@@ -676,24 +351,6 @@ const Composicaoselec = (props) => {
     });
   };
 
-  const Mapa = () => {
-    getZipCode(valuesfield[10]).then((response) => {
-      if (response.status === 200) {
-        try {
-          let tmplocation = response.data.results[0].geometry.location;
-          let locationfim = [];
-          tmplocation['name'] = valuesfield[11];
-          locationfim = locationfim.concat(tmplocation);
-          setLocation(locationfim);
-          setShowmapa(true);
-        } catch (error) {
-          setLocation([]);
-          setItemvariant(1);
-          setMensagem('Não foi possível buscar as coordenadas deste Endereço !');
-        }
-      }
-    });
-  };
 
   return (
     <React.Fragment>
@@ -743,23 +400,6 @@ const Composicaoselec = (props) => {
           </Card>
         </Row>
         <Row style={{ textAlign: 'center', marginTop: '1px', marginBottom: '1px' }}>
-          <Row>
-            {steps !== undefined && steps.length > 0 ? (
-              <Col>
-                {steps.map((label, index) => (
-                  <Button
-                    key={index}
-                    className={index === activeStep ? 'btn btn-success shadow-2 mb-3' : 'btn btn-primary shadow-2 mb-3'}
-                    onClick={handleStep(options[index])}
-                  >
-                    <i className={icons[index]} /> {label}
-                  </Button>
-                ))}
-              </Col>
-            ) : (
-              <></>
-            )}
-          </Row>
         </Row>
         {activeStep === 0 ? (
           <Card className="Recent-Users" style={{ marginBottom: '5px' }}>
@@ -785,36 +425,7 @@ const Composicaoselec = (props) => {
         ) : (
           <></>
         )}
-        {activeStep === 1 ? (
-          <Card className="Recent-Users" style={{ marginBottom: '5px' }}>
-            <Card.Header>
-              <Card.Title as="h5">Localização do Equipamento</Card.Title>
-            </Card.Header>
-            <Row style={{ marginTop: '5px', marginLeft: '5px', marginRight: '5px' }}>
-              {fieldsaddress.map((field, index) => (
-                <CreateObject
-                  key={index}
-                  field={field}
-                  index={field.id}
-                  fields={fieldsaddress}
-                  valuesfield={valuesfield}
-                  setValuesfield={(data) => setValuesfield(data)}
-                  valuesfield2={valuesfield2}
-                  setValuesfield2={(data) => setValuesfield2(data)}
-                  disabled={valuesdisable[field.id]}
-                ></CreateObject>
-              ))}
-              <Col style={{ marginTop: '28px' }}>
-                <Button className="btn btn-success  mb-2" onClick={(e) => Mapa()}>
-                  <i className={'feather icon-map'} />
-                  Visualizar Mapa
-                </Button>
-              </Col>
-            </Row>
-          </Card>
-        ) : (
-          <></>
-        )}
+        
         <hr></hr>
         <Row>
           <Alert
@@ -841,85 +452,8 @@ const Composicaoselec = (props) => {
             </Col>
           </Row>
         </Row>
-        <Modal backdrop="static" size="lg" show={showfile} centered={true} onHide={handleCloseShowFile}>
-          <div id="linear-progress">{carregando && <LinearProgress color="primary" />}</div>
-          <Modal.Header className="h5" closeButton>
-            <i className={'feather icon-camera h1'} />
-            &nbsp;Definição da Foto
-          </Modal.Header>
-          <ModalBody>
-            <Form.Group controlId="formFile" className="mb-1">
-              <Form.Label>Favor escolher o arquivo:</Form.Label>
-              <Form.Control
-                type="file"
-                onChange={(e) => {
-                  setUrl(URL.createObjectURL(e.target.files[0]));
-                  getBase64(e.target.files[0])
-                    .then((res) => {
-                      let pos = res.indexOf('base64') + 7;
-                      res = res.substring(pos);
-                      setImg64(res);
-                    })
-                    .catch((err) => console.log(err));
-                }}
-              />
-              <div style={{ textAlign: 'center', marginTop: '10px' }}>
-                {url !== undefined && (url !== null) & (url !== '') ? <img src={url} alt="avatar" width="416" height="416" /> : <></>}
-              </div>
-            </Form.Group>
-          </ModalBody>
-          <ModalFooter>
-            <Row style={{ textAlign: 'rigth' }}>
-              <Col>
-                <Button id="btnSalvar" className="btn btn-success shadow-2 mb-2" onClick={(e) => salvarFoto()}>
-                  <i className={'feather icon-save'} /> Salvar
-                </Button>
-                <Button id="btnCancelar" className="btn btn-warning shadow-2 mb-2" onClick={(e) => handleCloseShowFile()}>
-                  <i className={'feather icon-x'} />
-                  Cancelar
-                </Button>
-              </Col>
-            </Row>
-          </ModalFooter>
-        </Modal>
-        <Modal backdrop="static" size="xl" show={showmapa} centered={true} onHide={handleClosemapa}>
-          <Modal.Header className="h5" closeButton>
-            <i className={'feather icon-map'} />
-            &nbsp;Visualização do Mapa
-          </Modal.Header>
-          <ModalBody>
-            <Row
-              style={{
-                marginTop: '5px',
-                marginLeft: '5px',
-                marginRight: '5px',
-                marginBottom: '5px',
-                height: '540px',
-                textAlign: 'center'
-              }}
-            >
-              {location !== undefined && location.length > 0 ? (
-                <GoogleMap
-                  bootstrapURLKeys={{ key: Decode64(localStorage.getItem('apikey_maps')) }}
-                  defaultCenter={{ lat: location[0].lat, lng: location[0].lng }}
-                  defaultZoom={17}
-                >
-                  {location.map((item, index) => (
-                    <AnyReactComponent key={index} lat={item.lat} lng={item.lng} text={item.text} />
-                  ))}
-                </GoogleMap>
-              ) : (
-                <></>
-              )}
-            </Row>
-          </ModalBody>
-          <ModalFooter>
-            <Button id="btnFechar" className="btn btn-primary shadow-2 mb-2" onClick={handleClosemapa}>
-              <i className={'feather icon-x-circle'} />
-              Fechar
-            </Button>
-          </ModalFooter>
-        </Modal>
+        
+       
       </div>
     </React.Fragment>
   );
