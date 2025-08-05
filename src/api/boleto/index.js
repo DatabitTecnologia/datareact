65b6209a2631471cf30dc7551f839fd0f19a1ccd
Sidney <sistemas@databit.com.br>
2025-08-05 14:59:06 -0300

    Atividades 05/08/2025

--- src/api/boleto/index.js
+++ src/api/boleto/index.js
@@ -3,10 +3,11 @@ import { getInstance } from '../apiinstance';
 export const gerarBoleto = async (boletos) => {
   try {
     const apiInstance = getInstance();
-    const responseboleto = await apiInstance.post('gerarboleto', {
+    const responseboleto = await apiInstance.post('gerarboleto/', {
       boletos: boletos
     });
     const result = responseboleto.data;
+    console.log(result);
     if (result.status === 1 && result.mensagem) {
       const base64 = result.mensagem;
       const pdfBlob = await fetch(`data:application/pdf;base64,${base64}`).then((res) => res.blob());
@@ -16,4 +17,10 @@ export const gerarBoleto = async (boletos) => {
   } catch (error) {
     console.log(error);
   }
+};
+
+const decodeHtml = (html) => {
+  const txt = document.createElement('textarea');
+  txt.innerHTML = html;
+  return txt.value;
 };
